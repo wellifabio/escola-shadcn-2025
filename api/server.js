916c502e10b654edc29642c9581bc8540ed41c12
wellifabio <wellifabio@hotmail.com>
2025-10-22 08:02:36 -0300

    insomnia

--- api/server.js
+++ api/server.js
@@ -11,4 +11,4 @@ app.use(cors());
 app.use(express.json());
 app.use(rotes);
 
-app.listen(PORT, () => { console.log("API Escola destão de turmas em http://localhost:" + PORT) });
+app.listen(PORT, () => { console.log("API Escola, Gestão de turmas em http://localhost:" + PORT) });
